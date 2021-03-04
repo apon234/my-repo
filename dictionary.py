c75e44f6d14898dc1ccebb4d9a72bd0db43260ed
apon234 <apon.kumarr96@gmail.com>
2021-03-05 00:00:17 +0600

    dummy push

---
+++ dictionary.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  9 14:46:59 2021
+
+@author: Hp
+"""
+def controlDev():
+    i = 9.92
+    price = {"rice": 52.5, "salt": 35.75, "wheat": 32.25}
+    print(price)
+    
+    
+controlDev();
